e80c204797b46a9d331b259580d243168bf9da5e
gitgernit <petr.zakharov.anton@gmail.com>
2024-08-05 00:00:11 +0300

    :art:sparkles:feat(): type hints!

--- src/boilergram/core/management/inits.py
+++ src/boilergram/core/management/inits.py
@@ -2,23 +2,27 @@ import shutil
 import pathlib
 
 
-def copy_bot_template(target_dir):
+def copy_bot_template(target_dir: pathlib.Path) -> None:
     template_path = (pathlib.Path(__file__).parent.parent / 'templates').resolve()
 
     shutil.copytree(template_path, target_dir, dirs_exist_ok=True)
 
 
-def rename_bot_template(template_parent_dir, bot_name):
+def rename_bot_template(template_parent_dir: pathlib.Path,
+                        bot_name: str):
     bot_path = template_parent_dir / 'bot'
-
-    bot_path = bot_path.rename(bot_name).resolve()
+    bot_path = bot_path.rename(bot_name)
 
     inner_bot_path = bot_path / 'bot'
     inner_bot_path.rename(bot_path / bot_name)
 
 
-def create_new_bot(bot_name):
+def create_new_bot(bot_name: str):
     current_path = pathlib.Path.cwd().resolve()
 
     copy_bot_template(current_path)
     rename_bot_template(current_path, bot_name)
+
+
+if __name__ == '__main__':
+    create_new_bot('testbot')
