bc55f31f6eb0408d501e64de14444915ccb5a46f
gitgernit <petr.zakharov.anton@gmail.com>
2024-08-07 01:38:41 +0300

    :sparkles:feat(): PathSettings!
    PathSettings work alike Settings but are used by internals of boilergram to store inner paths (for example, PACKAGE_ROOT), pathlib-processed

---
+++ src/boilergram/conf/__init__.py
@@ -0,0 +1,24 @@
+__all__ = ['path_settings']
+
+import pathlib
+import boilergram.conf.paths
+
+
+class PathSettings:
+    def __init__(self, package_root):
+        self.PACKAGE_ROOT: pathlib.Path = package_root
+
+        for setting, value in (
+                (setting, getattr(boilergram.conf.paths, setting))
+                for setting in dir(boilergram.conf.paths)
+        ):
+            if setting.isupper() and '.' in value:
+                pathlike_value = value.replace('.', '/')
+                path_value = pathlib.Path(self.PACKAGE_ROOT / '/'.join(
+                    pathlike_value.split('/')[1:]
+                ))
+
+                setattr(self, setting, path_value)
+
+
+path_settings = PathSettings(pathlib.Path(__file__).parent.parent)
---
+++ src/boilergram/conf/paths.py
@@ -0,0 +1,1 @@
+PROJECT_TEMPLATE_MODULE = 'boilergram.core.templates.bot'
