43f3bb721620843cc30abbb30ea4974ccc791fae
gitgernit <petr.zakharov.anton@gmail.com>
2024-08-11 20:35:43 +0300

    :art:style(contrib): reformat boilergram.contrib.demo.routers
    changed single quotes to double to avoid escaping quotes; reformatted imports; type annotations

--- src/boilergram/contrib/demo/routers.py
+++ src/boilergram/contrib/demo/routers.py
@@ -1,6 +1,5 @@
-import aiogram.filters
 import aiogram.dispatcher
-
+import aiogram.filters
 
 demo_router = aiogram.Router()
 
@@ -12,31 +11,31 @@ demo_router.include_routers(command_router, text_router, other_types_router)
 
 
 @command_router.message(aiogram.filters.Command('start'))
-async def start(message: aiogram.types.Message):
+async def start(message: aiogram.types.Message) -> None:
     await message.answer('Welcome to boilergram!')
 
 
 @command_router.message(aiogram.filters.Command('dice'))
-async def dice(message: aiogram.types.Message):
+async def dice(message: aiogram.types.Message) -> None:
     await message.answer_dice()
 
 
 @text_router.message(aiogram.F.text)
-async def text(message: aiogram.types.Message):
+async def text(message: aiogram.types.Message) -> None:
     await message.answer(f'Gotcha, Mr. "{message}"!')
 
 
 @other_types_router.message()
-async def photo(message: aiogram.types.Message):
+async def photo(message: aiogram.types.Message) -> None:
     match message.content_type:
         case 'photo':
-            await message.answer('That\'s got to be a photo.')
+            await message.answer("That's got to be a photo.")
 
         case 'voice':
-            await message.answer('That\'s got to be a voice message.')
+            await message.answer("That's got to be a voice message.")
 
         case 'animation':
-            await message.answer('That\'s got to be an animation.')
+            await message.answer("That's got to be an animation.")
 
         case _:
             await message.answer('Not sure what that is.')
