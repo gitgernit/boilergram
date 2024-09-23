89dfabcd4ad5b8f37c43072530189b73dd8adf28
gitgernit <petr.zakharov.anton@gmail.com>
2024-09-24 02:44:05 +0300

    :white_check_mark::sparkles:test(cli): added filesystem operations with templates tests

---
+++ tests/test_cli/fixtures.py
@@ -0,0 +1,8 @@
+import pathlib
+
+import pytest
+
+
+@pytest.fixture(scope='class')
+def template_path(tmp_path_factory: pytest.TempPathFactory) -> pathlib.Path:
+    return tmp_path_factory.mktemp('template')
---
+++ tests/test_cli/unit/test_templates_fs.py
@@ -0,0 +1,33 @@
+import pathlib
+
+from boilergram.core.management.inits import copy_bot_template
+from boilergram.core.management.inits import rename_bot_template
+
+DESIRED_PATHS = [
+    'bot',
+    'bot/bot/',
+    'bot/bot/__init__.py',
+    'bot/bot/settings.py',
+    'bot/manage.py',
+]
+DESIRED_RENAMED_PATHS = [
+    'rbot',
+    'rbot/rbot/',
+    'rbot/rbot/__init__.py',
+    'rbot/rbot/settings.py',
+    'rbot/manage.py',
+]
+
+
+class TestTemplatesFilesystem:
+    def test_copy_bot_template(self, template_path: pathlib.Path) -> None:
+        copy_bot_template(template_path)
+
+        for path in DESIRED_PATHS:
+            assert (template_path / path).exists()
+
+    def test_rename_bot_template(self, template_path: pathlib.Path) -> None:
+        rename_bot_template(template_path, 'rbot')
+
+        for path in DESIRED_RENAMED_PATHS:
+            assert (template_path / path).exists()
